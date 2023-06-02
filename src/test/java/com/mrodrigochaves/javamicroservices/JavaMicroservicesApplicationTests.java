4bd45eaae3d7ba97afdcd1e874ae840a9f72790f
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-02 19:58:01 -0300

    add src

---
+++ src/test/java/com/mrodrigochaves/javamicroservices/JavaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrodrigochaves.javamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
