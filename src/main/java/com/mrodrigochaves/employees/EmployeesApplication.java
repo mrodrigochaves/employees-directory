4e7efe8255302261cb75f203b2f8b65ddff45c5a
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-03 19:46:36 -0300

    rename app class

--- src/main/java/com/mrodrigochaves/employees/EmployeesApplication.java
+++ src/main/java/com/mrodrigochaves/employees/EmployeesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Employees {
+public class EmployeesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Employees.class, args);
+		SpringApplication.run(EmployeesApplication.class, args);
 	}
 
 }
