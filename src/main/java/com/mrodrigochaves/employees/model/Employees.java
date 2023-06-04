5d87ad263398dccf8965ebde839cf3880007cbea
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-03 23:00:58 -0300

    add config

--- src/main/java/com/mrodrigochaves/employees/model/Employees.java
+++ src/main/java/com/mrodrigochaves/employees/model/Employees.java
@@ -1,5 +1,35 @@
 package com.mrodrigochaves.employees.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_employee")
+@Data
 public class Employees {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "lastname", nullable = false)
+    private String lastname;
+
+    @Column(name = "departament", nullable = false)
+    private String departament;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    private boolean available;
+
 }
