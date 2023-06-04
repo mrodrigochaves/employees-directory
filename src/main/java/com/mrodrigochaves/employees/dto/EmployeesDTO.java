43a864b6456abd99e295c29da0d3869ab49b017f
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-03 23:07:06 -0300

    Add dto

---
+++ src/main/java/com/mrodrigochaves/employees/dto/EmployeesDTO.java
@@ -0,0 +1,14 @@
+package com.mrodrigochaves.employees.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeesDTO {
+
+    private String name;
+    private String lastname;
+    private String departament;
+    private String title;
+    private boolean available;
+    
+}
