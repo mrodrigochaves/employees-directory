d24229895b31f404e19c6db9ae7e16578b0370bc
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-06 21:24:05 -0300

    config controller

--- src/main/java/com/mrodrigochaves/employees/controller/EmployeesController.java
+++ src/main/java/com/mrodrigochaves/employees/controller/EmployeesController.java
@@ -3,7 +3,6 @@ package com.mrodrigochaves.employees.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
