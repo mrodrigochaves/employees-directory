3a51e4c2221fed87f12e7a6e493261027b365cb9
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-06 21:32:04 -0300

    Add @Bean

--- src/main/java/com/mrodrigochaves/employees/EmployeesApplication.java
+++ src/main/java/com/mrodrigochaves/employees/EmployeesApplication.java
@@ -1,7 +1,12 @@
 package com.mrodrigochaves.employees;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.mrodrigochaves.employees.model.Employees;
+import com.mrodrigochaves.employees.repository.EmployeesRepository;
 
 @SpringBootApplication
 public class EmployeesApplication {
@@ -9,5 +14,23 @@ public class EmployeesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
 	}
+
+
+	@Bean
+	CommandLineRunner initDataBase(EmployeesRepository employeesRepository){
+		return args ->{
+			employeesRepository.deleteAll();
+			
+			Employees e = new Employees();
+			e.setName("José");
+			e.setLastname("Mourinho");
+			e.setDepartament("Desenvolvimento de Softwares");
+			e.setTitle("Senior Java Developer");
+
+			employeesRepository.save(e);
+		};
+
+	}
+
 
 }
