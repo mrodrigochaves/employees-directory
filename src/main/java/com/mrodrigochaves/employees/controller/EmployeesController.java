d3072737d31fcc7a485ed79e760918befad33db8
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-06 22:08:53 -0300

    update controller

--- src/main/java/com/mrodrigochaves/employees/controller/EmployeesController.java
+++ src/main/java/com/mrodrigochaves/employees/controller/EmployeesController.java
@@ -3,7 +3,6 @@ package com.mrodrigochaves.employees.controller;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import com.mrodrigochaves.employees.dto.EmployeesDTO;
 import com.mrodrigochaves.employees.service.EmployeesService;
 
@@ -25,64 +23,56 @@ import jakarta.validation.Valid;
 @RequestMapping("/employees")
 public class EmployeesController {
 
-    @Autowired
-    private EmployeesService service;
-    
-    @PostMapping
-    public ResponseEntity<EmployeesDTO> create(@RequestBody @Valid EmployeesDTO request){
-        Optional<EmployeesDTO> response = service.create(request);
-        if( response.isPresent()){
-            return new ResponseEntity<>(response.get(), HttpStatus.CREATED);   
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    private final EmployeesService service;
+
+    public EmployeesController(EmployeesService service) {
+        this.service = service;
     }
 
     @GetMapping
-    public ResponseEntity<List<EmployeesDTO>> getAll(){
+    public ResponseEntity<List<EmployeesDTO>> getAll() {
         return ResponseEntity.ok(service.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EmployeesDTO> getById(@PathVariable("id") Long id){
+    public ResponseEntity<EmployeesDTO> getById(@PathVariable("id") Long id) {
         Optional<EmployeesDTO> response = service.getById(id);
-
-        if( response.isPresent()){
-            return ResponseEntity.ok(response.get());
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return response.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/sobrenome/{nome}")
-    public ResponseEntity<EmployeesDTO> getByName(@PathVariable("name") String name){
+    @GetMapping("/lastname/{name}")
+    public ResponseEntity<EmployeesDTO> getByName(@PathVariable("name") String name) {
         Optional<EmployeesDTO> response = service.getByName(name);
-
-        if( response.isPresent()){
-            return ResponseEntity.ok(response.get());
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return response.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<EmployeesDTO> update(@PathVariable("id") Long id, @RequestBody @Valid EmployeesDTO request){
+    public ResponseEntity<EmployeesDTO> update(@PathVariable("id") Long id, @RequestBody @Valid EmployeesDTO request) {
         Optional<EmployeesDTO> response = service.update(id, request);
-
-        if( response.isPresent()){
-            return new ResponseEntity<>(response.get(), HttpStatus.CREATED);
-        }
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return response.map(dto -> new ResponseEntity<>(dto, HttpStatus.CREATED))
+                .orElse(ResponseEntity.badRequest().build());
     }
-    @DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Long id) {
-		service.delete(id);
-		return ResponseEntity.ok().build();
-	}
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> inactive(@PathVariable("id") Long id){
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/inactive/{id}")
+    public ResponseEntity<Void> inactive(@PathVariable("id") Long id) {
         boolean inactive = service.inactive(id);
         return inactive
-               ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
-               : new ResponseEntity<>(HttpStatus.NOT_FOUND);   
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
     }
 
+    @PostMapping
+    public ResponseEntity<EmployeesDTO> create(@RequestBody @Valid EmployeesDTO request) {
+        Optional<EmployeesDTO> response = service.create(request);
+        return response.map(dto -> new ResponseEntity<>(dto, HttpStatus.CREATED))
+                .orElse(ResponseEntity.badRequest().build());
+    }
 }
