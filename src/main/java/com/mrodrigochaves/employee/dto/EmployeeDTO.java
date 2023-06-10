6f032fae31be679d2ca775b341a83a6c957547d4
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-10 20:04:56 -0300

    config DTO

--- src/main/java/com/mrodrigochaves/employee/dto/EmployeeDTO.java
+++ src/main/java/com/mrodrigochaves/employee/dto/EmployeeDTO.java
@@ -1,15 +1,21 @@
 package com.mrodrigochaves.employee.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
-public record EmployeesDTO(
-    @JsonProperty("_id") Long id,
-    @NotBlank @NotNull String name,
-    @NotBlank @NotNull String lastname,
-    @NotBlank @NotNull String departament,
-    @NotBlank @NotNull String title) {
-    
+@Data
+public class EmployeeDTO {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String department;
+
+    @NotBlank
+    private String title;
+
 }
