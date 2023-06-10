d331be1cbd2b87bc8e86aaa968e1343662225c1b
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-10 20:04:31 -0300

    change application

---
+++ src/main/java/com/mrodrigochaves/employee/EmployeeApplication.java
@@ -0,0 +1,40 @@
+package com.mrodrigochaves.employee;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.mrodrigochaves.employee.model.Employee;
+import com.mrodrigochaves.employee.repository.EmployeeRepository;
+
+import jakarta.persistence.Id;
+
+
+
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner initDataBase(EmployeeRepository employeesRepository){
+		return args ->{
+			employeesRepository.deleteAll();
+			
+			Employee e = new Employee();
+			e.setName("José");
+			e.setLastName("Mourinho");
+			e.setDepartment("Desenvolvimento de Softwares");
+			e.setTitle("Senior Java Developer");
+
+			employeesRepository.save(e);
+		};
+
+	}
+
+}
