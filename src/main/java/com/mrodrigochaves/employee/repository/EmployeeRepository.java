215c6a6ccd52c25ac8585d42babc8cd3a1afaa87
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-10 20:06:52 -0300

    update repository

--- src/main/java/com/mrodrigochaves/employee/repository/EmployeeRepository.java
+++ src/main/java/com/mrodrigochaves/employee/repository/EmployeeRepository.java
@@ -1,7 +1,19 @@
 package com.mrodrigochaves.employee.repository;
 
-import com.mrodrigochaves.employee.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mrodrigochaves.employee.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+    List<Employee> findByLastName(String lastName);
+
+    List<Employee> findByDepartment(String department);
+
+    List<Employee> findByTitle(String title);
+    
 }
