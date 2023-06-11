c727e5218113332aabe468bf933b655655e7bd49
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-11 13:04:27 -0300

    update application

--- src/main/java/com/mrodrigochaves/employee/EmployeeApplication.java
+++ src/main/java/com/mrodrigochaves/employee/EmployeeApplication.java
@@ -26,9 +26,25 @@ public class EmployeeApplication {
 			e.setName("José");
 			e.setLastName("Mourinho");
 			e.setDepartment("Desenvolvimento de Softwares");
-			e.setTitle("Senior Java Developer");
+			e.setTitle("Java Developer");
 
 			employeesRepository.save(e);
+
+			Employee e2 = new Employee();
+			e2.setName("Márcio");
+			e2.setLastName("Rodrigo");
+			e2.setDepartment("Desenvolvimento de Softwares");
+			e2.setTitle("Java Developer");
+
+			employeesRepository.save(e2);
+
+			Employee e3 = new Employee();
+			e3.setName("Cristiano");
+			e3.setLastName("Ronaldo");
+			e3.setDepartment("Desenvolvimento de Softwares");
+			e3.setTitle("React Developer");
+
+			employeesRepository.save(e3);
 		};
 
 	}
