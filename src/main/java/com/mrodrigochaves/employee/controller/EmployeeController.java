1545b2d9ed819dddac4797ce0bcc58d15760823a
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-06-11 13:05:14 -0300

    refactoring controller

--- src/main/java/com/mrodrigochaves/employee/controller/EmployeeController.java
+++ src/main/java/com/mrodrigochaves/employee/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +22,10 @@ import com.mrodrigochaves.employee.service.EmployeeService;
 import jakarta.validation.Valid;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/employees")
 public class EmployeeController {
+
 
     @Autowired
     private final EmployeeService service;
